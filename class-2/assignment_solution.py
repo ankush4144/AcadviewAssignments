ad9d47eb1ec1bc6c7c7cac450149f403beab30ed
ankush41444144 <ankush1129.cse16@chitkara.edu.in>
2018-09-11 21:51:50 +0530

    Assignment of class 2

---
+++ class-2/assignment_solution.py
@@ -0,0 +1,32 @@
+#print something
+print("HOW ARE YOU")
+
+#join two strings
+str1='Ankush'
+str2='Goyal'
+print(str1+str2)
+
+#take value of three values and print their value on screen
+a=input("Enter 1st value")
+b=input("Enter 2nd value")
+c=input("Enter 3rd value")
+print(a)
+print(b)
+print(c)
+
+#Print Let’s get started on screen.
+print("Let’s get started")
+
+
+#Print the following values using placeholders. 
+s="Acadview"
+course="Python"
+fees=5000
+
+print(s,course,fees)
+
+#find area of circle
+pi=3.14
+r=int(input("Enter radius of circle"))
+print("Area of circle is",pi*r**2)
+
